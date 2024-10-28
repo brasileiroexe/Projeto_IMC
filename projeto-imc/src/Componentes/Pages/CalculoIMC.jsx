0d978dc95d328476b6b35c9c9970c0306c62501f
Tucuterio <murilo.ribeiro.lima11@gmail.com>
2024-10-28 17:31:52 -0300

    Paginas Criadas
    Criação das Paginas Home, CalculoIMC, Sobrenós e Histórico

---
+++ projeto-imc/src/Componentes/Pages/CalculoIMC.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { auth } from "../../Firebase.js";
+import { db } from "../../Firebase.js";
+import TabelaIMC from "./../TabelaIMC.jsx"; // Importa a Tabela de Classificação
+import Footer from "../Footer.js";
+import "./CalculoIMC.css";
+
+function CalculoIMC() {
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+  const [imc, setIMC] = useState(null);
+  const [msg, setMsg] = useState("");
+  const [showTabela, setShowTabela] = useState(false); // Controla a exibição da tabela
+
+  const calcularIMC = async () => {
+    if (!peso || !altura) {
+      setMsg("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    const calculo = (peso / (altura * altura)).toFixed(2);
+    setIMC(calculo);
+    setShowTabela(true); // Exibe a tabela após o cálculo
+
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        await addDoc(collection(db, "users", user.uid, "imcHistory"), {
+          weight: peso,
+          height: altura,
+          bmi: calculo,
+          date: new Date().toISOString(),
+        });
+        setMsg("IMC calculado e salvo no histórico!");
+      } else {
+        setMsg("Por favor, faça login para salvar o histórico.");
+      }
+    } catch (error) {
+      console.error(error);
+      setMsg("Erro ao salvar o histórico.");
+    }
+  };
+
+  return (
+    <div className="calculadora-imc-container">
+      <h2>Calcular IMC</h2>
+      <div className="input-group">
+        <input
+          type="number"
+          placeholder="Peso (kg)"
+          onChange={(e) => setPeso(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <input
+          type="number"
+          placeholder="Altura (m)"
+          onChange={(e) => setAltura(e.target.value)}
+        />
+      </div>
+      <button onClick={calcularIMC} className="calcular-button">
+        Calcular IMC
+      </button>
+      {imc && <p>Seu IMC é: {imc}</p>}
+      <p>{msg}</p>
+
+      {/* Exibe a tabela de classificação IMC */}
+      {showTabela && <TabelaIMC onClose={() => setShowTabela(false)} />}
+    </div>
+  );
+}
+
+export default CalculoIMC;
---
+++ projeto-imc/src/Componentes/Pages/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="content-box">
+      <div className="info-box">
+        <h2>Afinal, o que é Saúde?</h2>
+        <p>
+          Saúde é o estado de equilíbrio físico, mental e social de uma pessoa,
+          não se limitando apenas à ausência de doenças ou enfermidades. Ela
+          envolve o bem-estar total do corpo e da mente, a capacidade de
+          realizar atividades diárias, e uma harmonia entre o indivíduo e o
+          ambiente em que vive.
+        </p>
+      </div>
+      <div className="info-box">
+        <h2>Por que ter Sáude?</h2>
+        <p>
+          Ter saúde é fundamental porque ela é a base para viver de forma plena
+          e produtiva. Sem saúde, nossas capacidades físicas, mentais e
+          emocionais ficam comprometidas, o que limita nossa autonomia e
+          qualidade de vida. A saúde nos permite realizar atividades cotidianas,
+          enfrentar desafios, aproveitar momentos de lazer e construir
+          relacionamentos saudáveis.{" "}
+        </p>
+      </div>
+      <div className="info-box">
+        <h2>Como ter Saúde?</h2>
+        <ul className="list-box">
+          <li>Pra ter Sáude é importante:</li>
+          <li>1. Comer bem: Incluir frutas, verduras e junk food.</li>
+          <li>
+            2. Exercitar-se: Realizar atividades físicas, caminhadsa, treinos,
+            movimentação regularmente.
+          </li>
+          <li>
+            3. Cuidar da Mente: Relaxar, lazer, evitar estresse constante.
+          </li>
+          <li>
+            4. Dormir Bem: Ter noites de sono adequados para recuperar o corpo.
+          </li>
+          <li>5. Ir ao Médico: Fazer exames e previnir problemas de saúde.</li>
+          <li>
+            6. Ter bons Relacionamentos: Manter conexões sociais saudáveis.{" "}
+          </li>
+          <li>
+            Esses hábitos ajudam a manter o corpo e a mente saudáveis e
+            equilíbrados.
+          </li>
+        </ul>
+      </div>
+      <div className="info-box">
+        <h2>O que é IMC?</h2>
+        <p>
+          Ter saúde é fundamental porque ela é a base para viver de forma plena
+          e produtiva. Sem saúde, nossas capacidades físicas, mentais e
+          emocionais ficam comprometidas, o que limita nossa autonomia e
+          qualidade de vida. A saúde nos permite realizar atividades cotidianas,
+          enfrentar desafios, aproveitar momentos de lazer e construir
+          relacionamentos saudáveis.{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
